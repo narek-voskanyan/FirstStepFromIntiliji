85bd4a8951faf5c1ef962bc082d8683d02ad6d57
narek-voskanyan <narek.voskanyan91@gamil.com>
2022-03-14 16:46:03 +0400

    It is the main class, here we can call the method how is check IMEI`s box number

--- src/com/tray/Main.java
+++ src/com/tray/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Hello JAVA");
+        System.out.println("I had been added after the all code has been added");
     ArraylistVSLinkedList.outImeiBox();
+
 
     }
 }
